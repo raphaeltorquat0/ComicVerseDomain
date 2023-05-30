0e783e2a8b663c9e8d8df702e03afa7e10959230
Raphael Torquato <raphael.martini@live.com>
2023-05-30 18:42:55 -0300

    add textObject params
    add textObject params

--- Models/CoreEntity/Comics.swift
+++ Models/CoreEntity/Comics.swift
@@ -72,6 +72,12 @@ public struct CommicsModel: Codable {
 }
 
 
-public struct TextObject: Model {
+public struct TextObject: Codable {
+    public var type: String?
+    public var url: String?
     
+    public init(type: String?, url: String) {
+        self.type = type
+        self.url = url
+    }
 }
